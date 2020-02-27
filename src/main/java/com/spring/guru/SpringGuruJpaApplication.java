0a7370c605c38df6cc33140be90950f6d46e7147
Mahesh M <mahesh.m@centurylink.com>
2020-02-28 02:14:39 +0530

    My First Commit

---
+++ src/main/java/com/spring/guru/SpringGuruJpaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuruJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuruJpaApplication.class, args);
+	}
+
+}
